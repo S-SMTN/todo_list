42dd29a7c8a2f1d19bb6af2c889635f650f5d440
S-SMTN <cven28@gmail.com>
2024-05-26 17:00:03 +0300

    added models im models.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import Tag, Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    fields = ["content", "deadline", "tags"]
+    list_display = [
+        "content",
+        "datetime",
+        "deadline",
+        "is_completed",
+        "tag_list"
+    ]
+    list_filter = [
+        "datetime",
+        "is_completed",
+        "tags"
+    ]
+
+    def tag_list(self, obj: Task) -> str:
+        return ", ".join(
+            [str(tag) for tag in obj.tags.all()]
+        )
+
+
+admin.site.register(Tag)
