7220efd2d119e9afaf1fb6cf0ba44b936ae8bd0d
S-SMTN <cven28@gmail.com>
2024-05-26 17:00:27 +0300

    added admin settings

--- app/models.py
+++ app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    datetime = models.DateTimeField(
+        auto_now_add=True,
+        blank=True
+    )
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField(
+        to=Tag,
+        related_name="tasks"
+    )
+
+    class Meta:
+        ordering = ["is_completed", "deadline"]
+
+    def __str__(self) -> str:
+        return f"{self.content}"
+
