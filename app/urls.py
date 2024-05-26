9b818ba07666b435147b2a62e321f99b5af37306
S-SMTN <cven28@gmail.com>
2024-05-26 18:32:12 +0300

    implemented tag list page

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from app.views import IndexView
+from app.views import IndexView, TagListView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
 ]
 
 app_name = "app"
--- app/views.py
+++ app/views.py
@@ -1,9 +1,14 @@
 from django.views import generic
 
-from app.models import Task
+from app.models import Task, Tag
 
 
 class IndexView(generic.ListView):
     model = Task
     paginate_by = 10
     template_name = "app/index.html"
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    paginate_by = 10
